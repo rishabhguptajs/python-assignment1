ffdae1b579ecf8f3f23bf2e1b6a95eca1af10b66
rishabhguptajs <rishabhgupta4523@gmail.com>
2023-11-06 23:02:58 +0530

    .

---
+++ code4.py
@@ -0,0 +1,6 @@
+input_str = input("Enter a sequence of comma-separated numbers: ")
+number_list = input_str.split(',')
+number_tuple = tuple(number_list)
+
+print("List:", number_list)
+print("Tuple:", number_tuple)
